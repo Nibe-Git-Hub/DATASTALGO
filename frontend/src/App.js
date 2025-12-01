0c36ef38105eb407f8b5ea4e7bf592dce144c517
Nibe <neabella2@student.hau.edu.ph>
2025-12-01 14:18:22 +0800

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+
+import { Container } from 'react-bootstrap';
+
+import HomeScreen from './screens/HomeScreen';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} exact />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
